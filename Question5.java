924b7e6446237e658a53a45c42de6906d001d2df
tgonet <tanmingzhe.99@gmail.com>
2020-04-24 16:57:33 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,13 +26,54 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-   Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
     int repeat = in.nextInt();
-    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> inputlist = new ArrayList<>();
+    ArrayList<Integer> distinctlist = new ArrayList<>();
+    ArrayList<Integer> countlist = new ArrayList<>();
     while(repeat != 0){
       int input = in.nextInt();
-      list.add(input);
+      inputlist.add(input);
       repeat --;
+      if(!distinctlist.contains(input)){
+        distinctlist.add(input);
+      } 
+    }   
+    Collections.sort(inputlist);
+
+    int number = 0;
+    int count = 0;
+    int current = inputlist.get(0);
+    for(int j = 0;j<inputlist.size();j++){
+      if(inputlist.get(j) == current){
+        count += 1;
+      }
+      else if(j == inputlist.size()-1){
+        if(inputlist.get(j) == current){
+          count += 1;
+          countlist.add(count);
+        }
+        else{
+          countlist.add(count);
+          countlist.add(1);
+        }
+      }
+      else{
+        countlist.add(count);
+        count = 1;
+        current = inputlist.get(j);
+      }
+    }
+    int largest = 0;
+    int position = 0;
+    for(int z = 0;z < countlist.size();z++){
+      if(countlist.get(z) > largest){
+        largest = countlist.get(z);
+        position = z;
+      }
+     
+    }
+    System.out.println(distinctlist.get(position));
     } 
   }
 }
